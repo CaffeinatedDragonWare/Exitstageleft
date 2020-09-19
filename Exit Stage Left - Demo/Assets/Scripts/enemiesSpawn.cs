b18514ba40345d98fb97e26c69871574530204fa
TheDoomCider <lisajax@ymail.com>
2020-09-19 15:56:30 -0300

    Changes

---
+++ Exit Stage Left - Demo/Assets/Scripts/enemiesSpawn.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemiesSpawn : MonoBehaviour {
+  
+    // enemies
+    public GameObject lady01;
+    public GameObject dude01;
+  
+    Vector2 whereToSpawn;
+    public float spawnRate = 2f;
+    float nextSpawn = 0.0f;
+    float x = 2f; // x value
+    float y = 0.09f; // y value
+    int random = 0;
+    public static bool walkRight = false;
+    public static bool walkLeft = false;
+  
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+      
+      if (Time.time > nextSpawn) {
+        
+        x = x * -1; // enemy spawned on oppose side each time
+        if (x > 1) {
+          walkRight = false;
+          walkLeft = true;
+        }
+        
+        else if (x < 1) {
+          walkRight = true;
+          walkLeft = false;
+        }
+        
+        nextSpawn = Time.time + spawnRate; // controls amount of enemies spawned at a time
+        whereToSpawn = new Vector2 (x, y); // controls where the enemy is spawned
+        
+        if (random == 0) {
+          Instantiate (dude01, whereToSpawn, Quaternion.identity); // spawns enemies
+          random = 1;
+        }
+        
+        else if (random == 1) {
+          Instantiate (lady01, whereToSpawn, Quaternion.identity); // spawns enemies
+          random = 0;
+        }
+        
+      }
+    }
+}
---
+++ Exit Stage Left - Demo/Assets/Scripts/enemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour {
+  
+    public float speed;
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+    
+        if (enemiesSpawn.walkLeft == true) { // move left
+          transform.Translate(2 * Time.deltaTime * speed , 0f, 0f);
+          transform.localScale = new Vector2 (2,2);
+        }
+        
+        else if (enemiesSpawn.walkRight == true) { // move right
+          transform.Translate(-2 * Time.deltaTime * speed , 0f, 0f);
+          transform.localScale = new Vector2 (-2,2);
+        }
+    }
+}
