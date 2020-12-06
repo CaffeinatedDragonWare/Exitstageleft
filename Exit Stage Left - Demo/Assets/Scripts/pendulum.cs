bb11b0b5f5444016ae97e68a922e5291b1bf26a2
TheDoomCider <lisajax@ymail.com>
2020-12-06 16:00:48 -0400

    prop controls added

--- Exit Stage Left - Demo/Assets/Scripts/pendulum.cs
+++ Exit Stage Left - Demo/Assets/Scripts/pendulum.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class pendulum : MonoBehaviour {
-  
-    // Start is called before the first frame update
-    void Start() {
 
+  public static string command = "nothing";
+
+  void Start() {
+      var hinge = GetComponent<HingeJoint2D>();
+      var motor = hinge.motor;
+      hinge.useMotor = true;
+      motor.motorSpeed = 0;
+      motor.maxMotorTorque = 10000;
+      hinge.useLimits = false;
+      hinge.motor = motor;
+  }
+
+  void Update() {
+
+    if (command == "left") {
+      var hinge = GetComponent<HingeJoint2D>();
+      var motor = hinge.motor;
+      motor.motorSpeed = 100;
+      hinge.motor = motor;
     }
 
-    // Update is called once per frame
-    void Update() {
-
+    else if (command == "right") {
+      var hinge = GetComponent<HingeJoint2D>();
+      var motor = hinge.motor;
+      motor.motorSpeed = -100;
+      hinge.motor = motor;
     }
+
+    else if (command == "stop") {
+      var hinge = GetComponent<HingeJoint2D>();
+      var motor = hinge.motor;
+      motor.motorSpeed = 0;
+      hinge.motor = motor;
+    }
+
+    // else if (command == "reset") {
+    //   hinge.motor = motor;
+    // }
+
+  }
 }
---
+++ Exit Stage Left - Demo/Assets/Scripts/selection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void propDirection(string recordedCommand) {
+
+      pendulum.command = recordedCommand;
+      
+    }
+}
